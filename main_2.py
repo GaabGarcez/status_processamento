11bb8b1309878ba04f6d6d0bf8ded47c38c69fed
GaabGarcez <antoniogabrielgarcezbarbosa@gmail.com>
2025-02-04 10:05:05 -0300

    feat: Adiciona uma verificação sem flask

---
+++ main_2.py
@@ -0,0 +1,11 @@
+import psutil
+import time
+
+
+
+while True:
+    cpu_usage = psutil.cpu_percent(interval=1)  # Percentual de uso da CPU
+    ram_usage = psutil.virtual_memory().percent  # Percentual de uso da RAM
+    print(f"Percentual CPU: {cpu_usage}%")
+    print(f"Percentual de RAM: {ram_usage}%")
+    time.sleep(2)  # Atualiza a cada 1 segundo
