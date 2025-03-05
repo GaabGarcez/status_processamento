858c9ddc5bb74f7dbd5071aea9b3d884682815ce
GaabGarcez <antoniogabrielgarcezbarbosa@gmail.com>
2025-03-05 13:21:38 -0300

    feat: adiciona main_2.py com gpu nvidia

--- main_2.py
+++ main_2.py
@@ -1,18 +1,23 @@
 import psutil
 import time
-import GPUtil
+import pynvml
+
+# Inicializa a NVML (NVIDIA Management Library)
+pynvml.nvmlInit()
+gpu_count = pynvml.nvmlDeviceGetCount()
 
 while True:
     cpu_usage = psutil.cpu_percent(interval=1)  # Percentual de uso da CPU
     ram_usage = psutil.virtual_memory().percent  # Percentual de uso da RAM
-    
-    # Obtendo informações das GPUs
-    gpus = GPUtil.getGPUs()
-    for gpu in gpus:
-        gpu_usage = gpu.load * 100  # gpu.load retorna um valor entre 0 e 1
-        print(f"GPU {gpu.id} - Uso: {gpu_usage:.2f}%")
+
+    # Itera por cada GPU disponível
+    for i in range(gpu_count):
+        handle = pynvml.nvmlDeviceGetHandleByIndex(i)
+        util = pynvml.nvmlDeviceGetUtilizationRates(handle)
+        print(f"GPU {i} - Uso: {util.gpu}% - Memória: {util.memory}%")
     
     print(f"Percentual CPU: {cpu_usage}%")
     print(f"Percentual de RAM: {ram_usage}%")
+    print('-' * 40)
     
     time.sleep(0.5)  # Atualiza a cada 0.5 segundo
