0b60eeff7349e929e58f4f735947dd314a033826
GaabGarcez <antoniogabrielgarcezbarbosa@gmail.com>
2025-03-11 12:59:08 -0300

    fix: volta a primeira versão

--- main_2.py
+++ main_2.py
@@ -1,16 +1,9 @@
 import psutil
 import time
-import GPUtil
 
 while True:
     cpu_usage = psutil.cpu_percent(interval=1)  # Percentual de uso da CPU
     ram_usage = psutil.virtual_memory().percent  # Percentual de uso da RAM
-    
-    # Obtendo informações das GPUs
-    gpus = GPUtil.getGPUs()
-    for gpu in gpus:
-        gpu_usage = gpu.load * 100  # gpu.load retorna um valor entre 0 e 1
-        print(f"GPU {gpu.id} - Uso: {gpu_usage:.2f}%")
     
     print(f"Percentual CPU: {cpu_usage}%")
     print(f"Percentual de RAM: {ram_usage}%")
